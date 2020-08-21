49162c7c764eca4c0a8fd795fd66b9b4dc4bee34
varghese <varghese87es@gmail.com>
2020-08-21 09:59:00 +0530

    update as on 21-08-2020

--- src/app/components/ecart/productlist/productitem/productitem.component.ts
+++ src/app/components/ecart/productlist/productitem/productitem.component.ts
@@ -10,7 +10,7 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 })
 export class ProductitemComponent implements OnInit {
 
-  closeResult = '';
+  
 
   @Input() productItem:Product
 
@@ -24,22 +24,5 @@ export class ProductitemComponent implements OnInit {
     this.msgService.sendMsg(this.productItem)
   }
 
-  open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
-  }
-
+  
 }
